2f1f4b41a3e61dbaf14c27218e6facd3a937204a
GabrielGrozz <gabrielgrozz.net@gmail.com>
2024-08-09 17:38:49 -0300

    finishing form

--- src/components/CertificateForm.jsx
+++ src/components/CertificateForm.jsx
@@ -4,103 +4,121 @@ const CertificateForm = () => {
   return (
     <>
       <div className="form">
-        <form
-          className=" grid grid-cols-2 gap-3"
-          action=""
-        >
-          <div className=" col-span-2 field">
-            <label htmlFor="fiel_name">Nome completo</label>
+        <form className=" " action="">
+          <div className="grid grid-cols-10 gap-3">
+            <div className=" col-span-10 field">
+              <label htmlFor="fiel_name">Nome completo</label>
+              <input
+                className="input"
+                type="text"
+                name="name"
+                id="fiel_name"
+                placeholder="Nome completo"
+              />
+            </div>
+            <div className="col-span-5 field">
+              <label htmlFor="field_rg">Registro geral(RG)</label>
+              <input
+                className="input"
+                type="text"
+                name="rg"
+                id="field_rg"
+                placeholder="Registro geral(RG)"
+              />
+            </div>
+            <div className="col-span-5 field">
+              <label htmlFor="field_cpf">CPF</label>
+              <input
+                className="input"
+                type="text"
+                name="cpf"
+                id="field_cpf"
+                placeholder="CPF"
+              />
+            </div>
+            <div className="col-span-5 field">
+              <label htmlFor="field_birth_date">Data de nascimento</label>
+              <input
+                className="input "
+                type="date"
+                name="birthDate"
+                id="field_birth_date"
+                placeholder="Data de nascimento"
+              />
+            </div>
+            <div className="col-span-10 field">
+              <label htmlFor="fiel_m_name">Nome da mãe</label>
+              <input
+                className="input"
+                type="text"
+                name="Mname"
+                id="fiel_m_name"
+                placeholder="Nome da mãe"
+              />
+            </div>
+            <div className="col-span-10 field">
+              <label htmlFor="field_d_name">Nome do pai</label>
+              <input
+                className="input"
+                type="text"
+                name="Dname"
+                id="field_d_name"
+                placeholder="Nome do pai"
+              />
+            </div>
+            <div className="col-span-10 field">
+              <label htmlFor="field_email">Email</label>
+              <input
+                className="input"
+                type="email"
+                name="email"
+                id="field_email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="col-span-10 field">
+              <label htmlFor="field_phone">Telefone / WhatsApp</label>
+              <input
+                className="input"
+                type="text"
+                name="phone"
+                id="field_phone"
+                placeholder="Telefone / WhatsApp"
+              />
+            </div>
+            <div className="col-span-3">
+              <input
+                className="w-[1.8vw]"
+                type="checkbox"
+                name="certificate"
+                id="formCertificateGeral"
+              />
+              <label htmlFor="formCertificateGeral">Geral</label>
+            </div>
+            <div className="col-span-3">
+              <input
+                type="checkbox"
+                name=""
+                id="formCertificateGeralCriminal"
+              />
+              <label htmlFor="formCertificateGeralCriminal">
+                Geral criminal
+              </label>
+            </div>
+            <div className="col-span-3">
+              <input type="checkbox" name="" id="formCertificateGeralCivel" />
+              <label htmlFor="formCertificateGeralCivel">Geral cível</label>
+            </div>
+          </div>
+          <div className="flex justify-center">
             <input
-              className="input"
-              type="text"
-              name="name"
-              id="fiel_name"
-              placeholder="Nome completo"
+              className="col-span-5 bg-green-700 hover:bg-green-600 rounded-xl text-white text-lg px-6 h-[6vh] w-[20vw]"
+              type="submit"
+              name="submit"
+              id="submitBtn"
+              value={"Solicitar certidão"}
             />
           </div>
-          <div className=" field">
-            <label htmlFor="field_rg">Registro geral(RG)</label>
-            <input
-              className="input"
-              type="text"
-              name="rg"
-              id="field_rg"
-              placeholder="Registro geral(RG)"
-            />
-          </div>
-          <div className=" field">
-            <label htmlFor="field_cpf">CPF</label>
-            <input
-              className="input"
-              type="text"
-              name="cpf"
-              id="field_cpf"
-              placeholder="CPF"
-            />
-          </div>
-          <div className=" field">
-            <label htmlFor="field_birth_date">Data de nascimento</label>
-            <input
-              className="input "
-              type="date"
-              name="birthDate"
-              id="field_birth_date"
-              placeholder="Data de nascimento"
-            />
-          </div>
-          <div className="col-span-2 field">
-            <label htmlFor="fiel_m_name">Nome da mãe</label>
-            <input
-              className="input"
-              type="text"
-              name="Mname"
-              id="fiel_m_name"
-              placeholder="Nome da mãe"
-            />
-          </div>
-          <div className="col-span-2 field">
-            <label htmlFor="field_d_name">Nome do pai</label>
-            <input
-              className="input"
-              type="text"
-              name="Dname"
-              id="field_d_name"
-              placeholder="Nome do pai"
-            />
-          </div>
-          <div className="col-span-2 field">
-            <label htmlFor="field_email">Email</label>
-            <input
-              className="input"
-              type="email"
-              name="email"
-              id="field_email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="col-span-2 field">
-            <label htmlFor="field_phone">Telefone / WhatsApp</label>
-            <input
-              className="input"
-              type="text"
-              name="phone"
-              id="field_phone"
-              placeholder="Telefone / WhatsApp"
-            />
-          </div>
-          <input type="checkbox" name="certificate" id="formCertificateGeral" />
-          <label htmlFor="formCertificateGeral">Geral</label>
-          <input type="checkbox" name="" id="formCertificateGeralCriminal" />
-          <label htmlFor="formCertificateGeralCriminal">Geral criminal</label>
-          <input type="checkbox" name="" id="formCertificateGeralCivel" />
-          <label htmlFor="formCertificateGeralCivel">Geral cível</label>
-          <input
-            type="submit"
-            name="submit"
-            id="submitBtn"
-            value={"Solicitar certidão"}
-          />
-
         </form>
       </div>
     </>
